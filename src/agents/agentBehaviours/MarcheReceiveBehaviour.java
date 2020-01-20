eada5427bea60b0e4680fdbfd1ac7b37877847d2
Melinouye <45028795+Melinouye@users.noreply.github.com>
2020-01-20 12:57:31 +0100

    init marcheReceiveBehaviour

---
+++ src/agents/agentBehaviours/MarcheReceiveBehaviour.java
@@ -0,0 +1,20 @@
+package agents.agentBehaviours;
+
+import jade.core.behaviours.Behaviour;
+import jade.lang.acl.ACLMessage;
+
+public class MarcheReceiveBehaviour extends Behaviour{
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
