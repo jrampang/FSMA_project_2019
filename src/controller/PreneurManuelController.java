61e7cef02939be08db8b70dc247db18ee3614484
Melinouye <45028795+Melinouye@users.noreply.github.com>
2020-01-20 09:34:47 +0100

    add PreneurManuelController

---
+++ src/controller/PreneurManuelController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import model.Enchere;
+
+public class PreneurManuelController {
+	@FXML
+    private TableView<Enchere> tableView;
+	
+	@FXML
+	private TableColumn<Enchere, String> EnchereCol;
+	
+	@FXML
+	private TableColumn<Enchere, String> PrixCol;
+	
+	@FXML
+	private Button rencherir;
+	
+	private static ObservableList<Enchere> list = FXCollections.observableArrayList();
+
+	public ObservableList<Enchere> getList() {
+		return list;
+	}
+
+	public static void setList(ObservableList<Enchere> inputList) {
+		list = inputList;
+	}
+	
+	public static void addEnchere(Enchere e) {
+		list.add(e);
+	}
+	
+	@FXML
+	public void initialize() {
+		tableView.setItems(list);
+		EnchereCol.setCellValueFactory(cellData -> cellData.getValue().objetProperty());
+		PrixCol.setCellValueFactory(cellData -> cellData.getValue().prixProperty());
+	}
+}
